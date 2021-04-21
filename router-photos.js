1a0551af0fc863cf4ddc64badfdbd6400024161c
dheera <dheera@dheera.net>
2021-04-21 06:59:49 +0000

    album

--- router-photos.js
+++ router-photos.js
@@ -1,16 +1,14 @@
 const log = require('pino')({prettyPrint: true, level: 'debug'});
 const router = require('express').Router();
 const photos = require('./photos');
-const AWS = require('./aws');
-
-const awsBucket = "static2.dheera.net";
-const imgixDomain = "dheera.imgix.net";
-
-s3 = new AWS.S3({apiVersion: '2006-03-01'});
+const aws = require('./aws');
+const fs = require('fs');
+const config_photos = JSON.parse(fs.readFileSync('./config/photos.json', 'utf8'));
+const md5 = require('md5');
 
 let listPhotos = (albumName) => {
     return new Promise((resolve, reject) => {
-        s3.listObjects({Bucket: awsBucket, Prefix: "photos/" + albumName}, (err, data) => {
+        aws.s3.listObjects({Bucket: config_photos.bucket, Prefix: config_photos.prefix + albumName}, (err, data) => {
             if(err) {
                 console.log("Error", err);
 	        reject("Error" + err.Code);
@@ -21,7 +19,7 @@ let listPhotos = (albumName) => {
             }
 	    outputList = [];
 	    for (i in data.Contents) {
-		 filename = data.Contents[i].Key.replace("photos/", "");
+		 filename = data.Contents[i].Key.replace(config_photos.prefix, "");
 		 if(filename.indexOf(".jpg") !== -1) {
                      outputList.push(filename);
 		 }
@@ -37,12 +35,27 @@ router.get('/', (req, res) => res.render('photos/index.html'));
 
 router.get('/:albumName', (req, res) => {
     listPhotos(req.params["albumName"])
-        .then(images => res.render('photos/album.html', {
-            images: images,
-            imgixDomain: imgixDomain,
-	    title: "Title",
-	    description: "Lorem ipsum",
-        }));
+        .then(imagefilelist => {
+	    images = [];
+	    for (i in imagefilelist) {
+		src = imagefilelist[i] + "?w=3072";
+		checksum = md5(config_photos.imgixSecureToken + "/" + src);
+		src = "https://" + config_photos.imgixDomain + "/" + src + "&s=" + checksum;
+		thumbnail = imagefilelist[i] + "?w=250";
+		checksum = md5(config_photos.imgixSecureToken + "/" + thumbnail);
+		thumbnail = "https://" + config_photos.imgixDomain + "/" + thumbnail + "&s=" + checksum;
+		images.push({
+		    path: imagefilelist[i],
+		    thumbnail: thumbnail,
+		    src: src,
+		});
+            }
+	    res.render('photos/album.html', {
+                images: images,
+                title: "Title",
+                description: "Lorem ipsum",
+            })
+	});
 });
 
 router.get('/:albumName/:imageName', (req, res) => {
