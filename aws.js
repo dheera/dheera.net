d790313c4da53cab454ec1b207e7d7dc3f6ab640
dheera <dheera@dheera.net>
2021-04-21 06:59:55 +0000

    aws

--- aws.js
+++ aws.js
@@ -1,6 +1,13 @@
 const AWS = require('aws-sdk');
+const fs = require('fs');
+const config_aws = JSON.parse(fs.readFileSync('./config/aws.json', 'utf8'));
 
-AWS.config.update({region: "us-west-2"});
-AWS.config.loadFromPath("./credentials/aws.json");
+AWS.config.update({region: config_aws.region});
+AWS.config.loadFromPath("./config/aws.json");
 
-module.exports = AWS;
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+module.exports = {
+  s3: s3,
+  config_aws: config_aws,
+};
