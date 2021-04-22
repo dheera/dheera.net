dd1947d2ed6ec4cd579242fa170e9da1523c0dcc
dheera <dheera@dheera.net>
2021-04-22 23:31:35 +0000

    fa

--- router-redirects.js
+++ router-redirects.js
@@ -1,4 +1,12 @@
 const log = require('pino')({prettyPrint: true, level: 'debug'});
 const router = require('express').Router();
 
+router.get('/photos/places/calnight', (req, res) => res.redirect("/photos/calnight"));
+router.get('/photos/places/icelandthermal', (req, res) => res.redirect("/photos/icelandthermal"));
+router.get('/photos/abstract/dancelight', (req, res) => res.redirect("/photos/dancelight"));
+router.get('/photos/journeys/jinghangyunhe', (req, res) => res.redirect("/photos/jinghangyunhe"));
+router.get('/photos/journeys/lashioroad', (req, res) => res.redirect("/photos/lashioroad"));
+router.get('/photos/journeys/timetravelchina', (req, res) => res.redirect("/photos/timetravelchina"));
+router.get('/projects/blur', (req, res) => res.redirect("/posts/blur"));
+
 module.exports = router;
