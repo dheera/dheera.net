26928280f6aba75e85a5a4cb3a4231f7d343e3f4
dheera <dheera@dheera.net>
2021-04-23 03:43:24 +0000

    minify

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const express = require('express');
+const minifyHTML = require('express-minify-html');
 const nunjucks = require('nunjucks');
 const fs = require('fs');
 const log = require('pino')({prettyPrint: true, level: 'debug'});
@@ -12,6 +13,19 @@ nunjucks.configure('views', {
     autoescape: true,
     express: app
 });
+
+app.use(minifyHTML({
+    override: true,
+    exception_url: false,
+    htmlMinifier: {
+        removeComments: true,
+        collapseWhitespace: true,
+        collapseBolleanAttributes: true,
+        removeAttributeQuotes: false,
+        removeEmptyAttributes: false,
+        minifyJS: true,
+    },
+}));
 
 app.use('/', router);
 
