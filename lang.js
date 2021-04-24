a36f4f9b0ccff7bf501ddbaa10406d599e3b1739
dheera <dheera@dheera.net>
2021-04-24 01:41:33 +0000

    lang

--- lang.js
+++ lang.js
@@ -1,3 +1,25 @@
+// Simple, no-fuss, minimalistic i18n framework for Express for simple websites
+// where you don't need to have that many languages.
+//
+// by Dheera Venkatraman (https://dheera.net/)
+//
+// How to use:
+// const lang = require("./lang");
+// app.use(lang);
+//
+// Content:
+// Simply put multiple language code anywhere in your HTML as [!lang:text|lang:text!], e.g.
+// 
+// <h1>[!en:Hello|es:Hola|zh:哈嘍!]</h1>
+// [!en:This is a test|es:Esto es una prueba|zh:這是一個測試!]
+// <img src="[!en:graphic_en.jpg|es:graphic_es.jpg|zh:graphic_zh.jpg!]">
+// ...
+//
+// and that's it. It will read the user's Accept-Language header (based on their system setting) and
+// pick the best-matched text from each set.
+//
+// This works by hijacking res.write and res.send, so it will work fine with templating frameworks like Nunjucks.
+
 const acceptLanguageParser = require('accept-language-parser');
 
 let filtered = (content, acceptLanguage) => {
