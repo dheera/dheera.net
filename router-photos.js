f5a783881d8bfb1fe842e0b18cc1d95a86f5277b
dheera <dheera@dheera.net>
2021-04-27 07:38:12 +0000

    photos

--- router-photos.js
+++ router-photos.js
@@ -47,7 +47,7 @@ router.get('/:albumName', (req, res) => {
                 images.push({
                     path: album.imageFiles[i],
                     thumbnail: contentFetcher.getSignedImageURL(album.imageFiles[i], "w=300&h=300&fit=crop&q=50"),
-                    src: album.noWatermark ? contentFetcher.getSignedImageURL(album.imageFiles[i], "w=2048&h=2048&fit=fillmax&q=90") : contentFetcher.getSignedImageURL(album.imageFiles[i], "w=2048&h=2048&fit=fillmax&mark=/photos/_watermark/512.png&mark-w=200&mark-align=bottom,left&mark-pad=50&q=95"),
+                    src: (album.watermark === false) ? contentFetcher.getSignedImageURL(album.imageFiles[i], "w=2048&h=2048&fit=fillmax&q=90") : contentFetcher.getSignedImageURL(album.imageFiles[i], "w=2048&h=2048&fit=fillmax&mark=/photos/_watermark/512.png&mark-w=200&mark-align=bottom,left&mark-pad=50&q=95"),
                     srcInfo: contentFetcher.getSignedImageURL(album.imageFiles[i], "fm=json"),
                 });
             }
